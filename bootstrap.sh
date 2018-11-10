939df77b10bd8db6b41ef65b964f8cdfd6dd3d20
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-09 21:52:46 -0600

    update pip

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 sudo pkg update &&
     sudo pkg install -y python3 ca_root_nss &&
     sudo pkg clean -y &&
-    sudo python3 -m ensurepip
+    sudo python3 -m ensurepip &&
+    sudo pip3 install --upgrade pip
